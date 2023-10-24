61326c01571ddbd8c324cc01f0b623dd6f999f6d
Henrik Markmanrud <henrik.markmanrud@student.nhh.no>
2023-10-24 10:03:21 +0200

    Did some changes to the table and simplified solution

--- timer.R
+++ timer.R
@@ -1,43 +1,59 @@
 library(tictoc)
 
+# The three libraries below are used in the other files, and they will have 
+# an impact on the elapsed running time. There they are loaded here in case
+# the solution files are not run in advance
+library(foreach) 
+library(iterators)
+library(parallel)
+
 # File for timing three different solutions
+
+# Clearing timing log
+
+tic.clearlog()
 
 # 1. Timing the solution 1 script as is
 
 tic("Timing solution 1")
 source("scripts/hw4_solution.r")
-Elasped_time_S1 <- toc()
-elapsed1 <- Elasped_time_S1$callback_msg
-
-# Timing for the first solution was approximately 90 seconds
+toc(log = TRUE)
 
 # 2. Timing the solution 2 script (using parallel computing)
 
 tic("Timing solution 2")
 source("scripts/hw4_solution2.r")
-Elasped_time_S2 <- toc()
-elapsed2 <- Elasped_time_S2$callback_msg
-
-# Timing for the second solution was approximately 65 seconds
+toc(log = TRUE)
 
 # 3. Timing the solution 3 script (split the M simulations in more than one core)
 
 tic("Timing solution 3")
 source("scripts/hw4_solution3.r")
-Elasped_time_S3 <- toc()
-elapsed3 <- Elasped_time_S3$callback_msg
+toc(log = TRUE)
 
-# Timing for the third solution was approximately 40 seconds
+printTicTocLog <- 
+  function() {
+    tic.log() %>% 
+      unlist %>% 
+      tibble(logvals = .) %>% 
+      separate(logvals, 
+               sep = ":", 
+               into = c("Solutions", "log")) %>% 
+      mutate(log = str_trim(log)) %>% 
+      separate(log,
+               sep = " ",
+               into = c("Seconds"),
+               extra = "drop")
+  }
 
-# Visualizing the three solution run times in a table
-times <- data.frame(
-  Time = c(elapsed1, elapsed2, elapsed3)
-)
-cat(times$Time, sep="\n")
+# Printing the results
+
+printTicTocLog() %>% 
+  knitr::kable()
 
 # Comment:
-# The third solution script was the fastest with roughly 40 seconds, compared
-# to the original solution using almost 90 seconds to run the code.
+# The third solution script was the fastest with roughly 23 seconds, compared
+# to the original solution using almost 43 seconds to run the code.
 # Solution 3 likely has the lowest running time because it parallelizes the 
 # "M" simulations, which might be more computationally intensive and therefore
 # yields a higher speedup when parallelized.
